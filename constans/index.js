721e2c90f36e301d4ae9aa132dc4c157939f574a
JASSIM ALHUMAID <jassim4421@outlook.sa>
2025-07-30 17:56:13 +0300

    cocktails section comlpeted

--- constans/index.js
+++
@@ -1,6 +0,0 @@
-export const navLinks = [
-    { id: "cocktails", title: "Cocktails" },
-    { id: "about", title: "About Us" },
-    { id: "the art", title: "The Art" },
-    { id: "contact", title: "Contact" },
-];
